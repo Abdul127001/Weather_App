680a62862d824cbf45ef564ca0a8986fb3558c42
Khan Abdul Karim <77256565+Abdul127001@users.noreply.github.com>
2025-03-15 12:35:44 +0530

    fetching hourly data

--- script.js
+++ script.js
@@ -11,21 +11,28 @@ const weatherCodes = {
     thunder_rain:[1273,1276]
 }
 
+
+const displayHourlyForecast = (h_data) => {
+
+}
+
 InputValue.addEventListener('keyup',(e)=>{
     // console.log(e.key)
 const API_KEY = "2ce08207c8d945bbbc463740251403";
 getWeatherDetails = async (c_name) => {
 // console.log(c_name)
-API_LINK = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${c_name}`;
+API_LINK = `http://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${c_name}&days=2`;
 
 try {
     const response = await fetch(API_LINK);
     const data = await response.json();
     console.log(data)
-    const temp = Math.floor(data.current.temp_c);
-    const desc = data.current.condition.text;
-    const weatherIMG = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(data.current.condition.code));
-    console.log(weatherIMG)
+    const temp = Math.floor(data?.current?.temp_c);
+    const desc = data?.current?.condition?.text;
+    const weatherIMG = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(data?.current?.condition?.code));
+    const combinedDays = [...data?.forecast?.forecastday[0]?.hour, ...data?.forecast?.forecastday[1]?.hour]
+    // console.log(combinedDays)
+    displayHourlyForecast(combinedDays);
     CurrWeather.querySelector('.weather-img').src = `images/animated/${weatherIMG}.svg`;
     CurrWeather.querySelector('.temprature').innerHTML = `${temp}<span>℃</span>`;
     CurrWeather.querySelector('.description').innerText = `${desc}`;
